cacf127000c10ee859884ed740fb71271610cf0c
zhugaru <zzzhugaru@gmail.com>
2022-11-14 02:55:01 +0300

    heroku.yml is added

--- src/main/java/ru/zhugaru/wordfckr/WordFckrBotApplication.java
+++ src/main/java/ru/zhugaru/wordfckr/WordFckrBotApplication.java
@@ -1,7 +1,9 @@
 package ru.zhugaru.wordfckr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class WordFckrBotApplication {
